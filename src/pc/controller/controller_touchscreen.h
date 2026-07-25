b81a79b5fd36f7cdf643dcecaf3f1e72ecc4256c
Toad114514 <xiaolan2332021@163.com>
2026-07-26 01:00:11 +0800

    [build] 兄啊你什么时候消失的
    CONTROL_ELEMENT_HIDDEN 变成棍母。

--- src/pc/controller/controller_touchscreen.h
+++ src/pc/controller/controller_touchscreen.h
@@ -34,6 +34,7 @@ enum ConfigControlElementAnchor {
     CONTROL_ELEMENT_LEFT,
     CONTROL_ELEMENT_RIGHT,
     CONTROL_ELEMENT_CENTER,
+    CONTROL_ELEMENT_HIDDEN,
 };
 
 enum ConfigControlElementIndex {
