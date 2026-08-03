09668b2468ef1ff4f049d308fae67be96c4aad28
Toad114514 <xiaolan2332021@163.com>
2026-08-03 23:14:34 +0800

    reverbx

--- src/pc/platform.h
+++ src/pc/platform.h
@@ -7,7 +7,7 @@
 
 /* platform-specific functions and whatnot */
 #ifdef TARGET_ANDROID
-#define ANDROID_APPNAME "com.shangshanruo666.sm64coopdxcnsv"
+#define ANDROID_APPNAME "com.excoopdxteam.sm64excoop"
 #endif
 
 #ifdef _WIN32
